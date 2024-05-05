394c6baede23eab4449197cbf3af82b27e5dde9a
Abnonymou <168055650+Abnonymou@users.noreply.github.com>
2024-05-05 15:21:55 +0800

    Update autolink.js

--- scripts/cmds/autolink.js
+++ scripts/cmds/autolink.js
@@ -71,7 +71,7 @@ module.exports = {
       } else {
         api.sendMessage("", event.threadID, event.messageID);
       }
-      api.setMessageReaction("💐", event.messageID, (err) => {}, true);
+      api.setMessageReaction("⬇️", event.messageID, (err) => {}, true);
     }
   },
   downLoad: function (url, api, event) {
