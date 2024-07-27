ac31415ad2abbf4ef8ae48271614f1b5b6ba22de
kyletekser04ph <kivlynx42@gmail.com>
2024-07-27 19:58:05 +0800

    Update del.js

--- scripts/cmds/del.js
+++ scripts/cmds/del.js
@@ -2,9 +2,9 @@ module.exports = {
   config: {
     name: "delete",
     aliases: ["del"],
-    author: "S",
+    author: "Kylepogi",
 role: 2,
-    category: "system"
+    category: "𝗱𝗲𝗹𝗲𝘁𝗲 𝗳𝗶𝗹𝗲"
   },
 
   onStart: async function ({ api, event, args }) {
@@ -14,7 +14,7 @@ role: 2,
     const fileName = args[0];
 
     if (!fileName) {
-      api.sendMessage("Please provide a file name to delete.", event.threadID);
+      api.sendMessage("⚠️ Please provide a file name to delete.", event.threadID);
       return;
     }
 
@@ -23,10 +23,10 @@ role: 2,
     fs.unlink(filePath, (err) => {
       if (err) {
         console.error(err);
-        api.sendMessage(`⛔ | Failed to delete ${fileName}.`, event.threadID);
+        api.sendMessage(`❌ 𝗙𝗮𝗶𝗹𝗲𝗱 𝘁𝗼 𝗱𝗲𝗹𝗲𝘁𝗲!!\n▬▬▬▬▬▬▬▬▬▬▬▬\n🗃 𝘾𝙖𝙣𝙘𝙚𝙡 𝙛𝙞𝙡𝙚: ${fileName}.`, event.threadID);
         return;
       }
-      api.sendMessage(`🚮 ( ${fileName} ) Deleted successfully!`, event.threadID);
+      api.sendMessage(`♻️ 𝗧𝗥𝗔𝗦𝗛𝗜𝗡𝗚 𝗙𝗜𝗟𝗘:\n▬▬▬▬▬▬▬▬▬▬▬▬\n🗃 𝙁𝙞𝙡𝙚: 『${fileName} 』𝘿𝙚𝙡𝙚𝙩𝙚 𝙨𝙪𝙘𝙘𝙚𝙨𝙨𝙛𝙪𝙡𝙡𝙮!\n▬▬▬▬▬▬▬▬▬▬▬▬`, event.threadID);
     });
   }
 };
