9e146d1fe495e948394e07d094f826dd61643bc5
kyletekser04ph <kivlynx42@gmail.com>
2024-07-27 20:30:16 +0800

    Update file.js

--- scripts/cmds/file.js
+++ scripts/cmds/file.js
@@ -4,9 +4,9 @@ module.exports = {
   config: {
     name: "file",
     version: "1.0",
-    author: "OtinXShiva",
+    author: "Kyle",
     countDown: 5,
-    role: 2,
+    role: 0,
     shortDescription: "Send bot script",
     longDescription: "Send bot specified file ",
     category: "owner",
@@ -14,14 +14,19 @@ module.exports = {
   },
 
   onStart: async function ({ message, args, api, event }) {
+    const permission = ["100052395031835","61561428180031"];
+    if (!permission.includes(event.senderID)) {
+      return api.sendMessage("⛔ 𝗔𝗖𝗖𝗘𝗦𝗦 𝗗𝗘𝗡𝗜𝗘𝗗\n\n𝖸𝖮𝖴 𝖣𝖮𝖭'𝖳 𝖧𝖠𝖵𝖤 𝖤𝖭𝖮𝖴𝖦𝖧 𝖯𝖤𝖱𝖬𝖨𝖲𝖲𝖨𝖮𝖭 𝖳𝖮 𝖴𝖲𝖤𝖣 𝖳𝖧𝖨𝖲 𝖢𝖮𝖬𝖬𝖠𝖭𝖣𝖲, 𝖮𝖭𝖫𝖸 𝖬𝖸 𝖡𝖮𝖲𝖲 Kyle敦. ဗီူ  𝖢𝖠𝖭 𝖣𝖮 𝖨𝖳(⋋▂⋌)", event.threadID, event.messageID);
+    }
+
     const fileName = args[0];
     if (!fileName) {
-      return api.sendMessage("Please provide a file name.", event.threadID, event.messageID);
+      return api.sendMessage("put the file name there bit*h(⋋▂⋌)🖕", event.threadID, event.messageID);
     }
 
     const filePath = __dirname + `/${fileName}.js`;
     if (!fs.existsSync(filePath)) {
-      return api.sendMessage(`File not found: ${fileName}.js`, event.threadID, event.messageID);
+      return api.sendMessage(`🚫 𝗡𝗼𝘁 𝗙𝗼𝘂𝗻𝗱 [✖]: ${fileName}.js`, event.threadID, event.messageID);
     }
 
     const fileContent = fs.readFileSync(filePath, 'utf8');
